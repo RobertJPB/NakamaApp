e005133d426a8f57ada48449253f6ad8160bb2e7
RobertJPB <robert@example.com>
2026-08-06 13:25:56 -0400

    commit

---
+++ api/_tmp_translate.ts
@@ -0,0 +1,15 @@
+import { translateText } from './src/infrastructure/external/kitsu/KitsuService'
+
+async function main() {
+  const res = await fetch('https://kitsu.io/api/edge/anime/12', { headers: { Accept: 'application/vnd.api+json' } })
+  const data = await res.json() as any
+  const sinopsisEn = data.data?.attributes?.synopsis || ''
+  console.log('Largo sinopsis EN:', sinopsisEn.length)
+
+  const t = await translateText(sinopsisEn)
+  console.log('Traduccion:', t ? t.slice(0, 200) : 'NULL')
+  if (t && /QUERY|MAX ALLOWED/i.test(t)) console.log('!!! ERROR PRESENTE EN TRADUCCION')
+  else console.log('OK: sin error')
+}
+
+main().catch(e => { console.error(e); process.exit(1) })
